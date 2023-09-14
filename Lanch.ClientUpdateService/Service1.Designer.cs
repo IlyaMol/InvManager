94e6e959ca34e2f4886665b3b51e6215cdf5460e
Илья Молявко <ilyamol.home@gmail.com>
2023-09-14 16:05:34 +0300

    Initial

---
+++ Lanch.ClientUpdateService/Service1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Lanch.ClientUpdateService
+{
+    partial class ClientUpdateService
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientUpdateService));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Client updater";
+            this.notifyIcon1.Visible = true;
+            // 
+            // ClientUpdateService
+            // 
+            this.ServiceName = "ClientUpdateService";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
